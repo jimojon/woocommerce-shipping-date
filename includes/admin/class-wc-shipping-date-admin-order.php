b37f768b3921f43178c73633a4f4c383a6d7b47f
Jonas Monnier <jonas@positronic.fr>
2023-07-28 23:14:32 +0200

    Bump version

--- includes/admin/class-wc-shipping-date-admin-order.php
+++ includes/admin/class-wc-shipping-date-admin-order.php
@@ -11,6 +11,7 @@ class WC_Shipping_Date_Admin_Order {
 
 	/**
 	 * Initialize the admin order actions.
+     * @since 0.4
 	 */
 	public function __construct() {
         add_action( 'woocommerce_admin_order_data_after_order_details', array( $this, 'action_woocommerce_admin_order_data_after_order_details' ), 10, 1 );
@@ -20,6 +21,7 @@ class WC_Shipping_Date_Admin_Order {
      * Add "Show order confirmation page" control.
      *
      * @param $order
+     * @since 0.4
      */
     public function action_woocommerce_admin_order_data_after_order_details( $order ) {
         echo '<a href="' . $order->get_checkout_order_received_url() . '" target="_blank" class="button" style="margin-top:20px; margin-bottom: 15px">' . __( 'Show order confirmation page', 'woocommerce-shipping-date' ) . '</a>';
